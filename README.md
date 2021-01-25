2cc3f2f11f9cece6e18acc574fa7d8da5b4844e9
Kamil Banach <kontakt@elwin013.com>
2021-01-25 22:43:24 +0100

    init with 1.0.0 version

---
+++ README.md
@@ -0,0 +1,66 @@
+# parcel-reporter-static-files-copy
+
+ParcelJS v2 plugin to copy static files from some directory to output dir. There is no watcher - 
+files are copied on build finish.
+
+
+## Install
+
+```
+yarn add parcel-reporter-static-files-copy --dev
+```
+
+```
+npm install -D parcel-reporter-static-files-copy
+```
+
+## Usage
+
+1. Create `static` directory in you project root.
+2. Fill it with your static files
+3. Add plugin to `.parcelrc`:
+```
+{
+  "extends": ["@parcel/config-default"],
+  "reporters":  ["...", "parcel-reporter-static-files-copy"]
+}
+```
+4. Run build - and that's all!
+
+## Customization
+
+Beyond the default settings, you can:
+
+1. Name of the directory to be copied.
+
+```json
+// package.json
+  {
+	...
+  "staticFiles": {
+    "staticPath": "customStatic"
+  }
+```
+
+### Additional example
+
+Check [examples](https://github.com/elwin013/parcel-reporter-static-files-copy/tree/master/examples) directory for 
+additional example. 
+
+## Flaws and problems
+
+At this moment this plugin copies all static files from some static dir to output (dist) directory. There is no 
+watcher on static files - only trigger is finishing build (no matter if normal build or serving).
+
+## Contribute
+
+You're interested in contributing? Awesome! Fork, make change, commit and create pull request. I'll do my best to merge 
+changes!
+
+## Thanks and acknowledgement
+
+Special thanks to [@gmougeolle](https://github.com/gmougeolle/).
+
+## License
+
+[MIT](/LICENSE)
---
+++ index.js
@@ -0,0 +1,77 @@
+'use strict';
+const { Reporter } = require('@parcel/plugin');
+const fs = require('fs');
+const path = require('path');
+
+const PACKAGE_JSON_SECTION = 'staticFiles';
+const DEFAULT_CONFIG = {
+    staticPath: ['static'],
+};
+
+const staticCopyPlugin = new Reporter({
+    async report({ event, options }) {
+        if (event.type === 'buildSuccess') {
+            let config = Object.assign(
+                {},
+                DEFAULT_CONFIG,
+                getSettings(options.projectRoot)
+            );
+
+            let distDir = options.distDir || 'dist';
+            copyDir(
+                options.projectRoot + '/' + config.staticPath,
+                options.projectRoot + '/' + distDir
+            );
+        }
+    },
+});
+
+const copyDir = (copyFrom, copyTo) => {
+    const copy = (filepath, relative, filename) => {
+        const dest = filepath.replace(copyFrom, copyTo);
+        if (!filename) {
+            if (!fs.existsSync(dest)) {
+                fs.mkdirSync(dest, { recursive: true });
+            }
+        } else {
+            fs.copyFileSync(filepath, dest);
+        }
+    };
+    recurseSync(copyFrom, copy);
+};
+
+/**
+ * Recurse into directory and execute callback function for each file and folder.
+ *
+ * Based on https://github.com/douzi8/file-system/blob/master/file-system.js#L254
+ *
+ * @param dirpath directory to start from
+ * @param callback function to be run on every file/directory
+ */
+const recurseSync = (dirpath, callback) => {
+    const rootpath = dirpath;
+
+    function recurse(dirpath) {
+        fs.readdirSync(dirpath).forEach(function (filename) {
+            const filepath = path.join(dirpath, filename);
+            const stats = fs.statSync(filepath);
+            const relative = path.relative(rootpath, filepath);
+
+            if (stats.isDirectory()) {
+                callback(filepath, relative);
+                recurse(filepath);
+            } else {
+                callback(filepath, relative, filename);
+            }
+        });
+    }
+
+    recurse(dirpath);
+};
+
+const getSettings = (projectRoot) => {
+    let packageJson = JSON.parse(fs.readFileSync(projectRoot + '/package.json'));
+    return Object.assign({}, packageJson[PACKAGE_JSON_SECTION]);
+};
+
+exports.default = staticCopyPlugin;
